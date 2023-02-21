3790b048240b91adfef3389210dd3af335ae272d
miraj <miraj00@gmail.com>
2023-02-21 18:04:55 -0500

    another option for retirement-info controller

--- src/main/java/co/grandcircus/assessment6/employeeController.java
+++ src/main/java/co/grandcircus/assessment6/employeeController.java
@@ -21,8 +21,8 @@ public class employeeController {
 	public String home(Model model) {
 
 		List<Employee> employee;
-		employee = repo.findAll(Sort.by("id"));
-		
+	//	employee = repo.findAll(Sort.by("id"));
+		employee = repo.findAll();
 		model.addAttribute("employee", employee);
 		
 		return "home";		
@@ -32,25 +32,35 @@ public class employeeController {
 	public String retirementInfo(
 			@RequestParam Long id,
 			Model model) {
-			
+/*	Option 1 : 		
 			Employee employeeinfo = repo.findById(id).get();
-		 
-			String display;
-			
 			System.out.println(employeeinfo);
-			
-						
+			String display;
 			if (employeeinfo.getAge() >= 65) {
-				
 				display = "Congratulations on your retirement! "
 						+ "		 You're entitled to $ " + ( (employeeinfo.getSalary() * 60)/100)  + "  in benefits! "; 
 			} else  {
-				
 				display = " Keep going! You're " + (65 - employeeinfo.getAge()) + " years away from retirement! ";
 			}
-			
 			model.addAttribute("message", display);
-			
+*/			
+// Option 2 : 
+		
+		Employee employee = repo.findById(id).orElse(null);
+		model.addAttribute("employee", employee);
+		
+		if (employee == null) {
+			model.addAttribute("message", "Employee not found.");
+		
+		} else if (employee.getAge() >= 65) {
+			int benefits = (int) Math.round(employee.getSalary() * .6);
+			model.addAttribute("message", "Congratulations on your retirement! You're entitled to $" + benefits + " in benefits!");
+		
+		} else {
+			int yearsLeft = 65 - employee.getAge();
+			model.addAttribute("message", "Keep going! You're " + yearsLeft + " years away from retirement!");
+		}	
+						
 		return "retirement-info";	
 	}
 		
